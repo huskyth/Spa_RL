e7b242bc7cbc7b671147561bb2a9faf04c9f8086
“huskyth” <1622313177@qq.com>
2026-07-22 22:24:17 +0800

    feat: some fix

--- sft/evaulate.sh
+++ sft/evaulate.sh
@@ -1,7 +1,7 @@
-python evaluate.py \
+python sft/evaluate.py \
     --base_model_name_or_path './models/Llama-3.2-3B-Instruct' \
-    --lora_adapter_path './ckpt/llama3b_webshop_sft/checkpoint-1000' \
+    --lora_adapter_path './ckpt/llama3b_webshop_sft/checkpoint-700' \
     --test_data_path 'data/webshop_sft_test.json' \
     --model_max_length 2048 \
-    --batch_size 8 \
+    --batch_size 4 \
     --flash_attn  # 如果训练时使用了 FlashAttention，评估时建议保持一致
